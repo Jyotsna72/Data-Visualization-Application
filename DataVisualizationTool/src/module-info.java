1a8330d574e7a5115646033e0473ab1b0a4daa2f
Jyotsna72 <160842793+Jyotsna72@users.noreply.github.com>
2026-06-26 19:24:00 +0530

    Delete DataVisualizationTool directory

--- DataVisualizationTool/src/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.datavisualization {
-    requires java.desktop;  // For GUI components
-    requires org.jfree.jfreechart;
-    requires org.jfree.jcommon;
-
-    // Expose packages to other modules if needed
-    exports com.datavisualization;
-}
